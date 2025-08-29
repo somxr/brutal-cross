687dc158ff347cc078facfbb4cb30ced2dd0bc84
Somar Jaafar <somar.jaafar19@gmail.com>
2025-08-29 19:56:24 -0400

    testing angular velocity

--- player/skate.gd
+++ player/skate.gd
@@ -7,7 +7,7 @@ class_name Skate
 @export var TURN_SPEED = 2.0
 ## How fast a character turns from direction it's facing moving to the direction the player is inputing. Circular speed.
 @export var ANGULAR_SPEED = 13.0
-
+@export var ANGULAR_SPEED_FROM_IDLE = 30.0
 @export var ACCELERATION := 3.0 # meters per second^2
 @export var DECELERATION := 4.0
 
@@ -36,11 +36,9 @@ func update(input: InputPackage, delta: float):
 	#player.velocity = velocity_by_input(input, delta)
 	#skate_velocity(input, delta)
 	visual.look_at(player.global_position - player.velocity.normalized())
-	player.move_and_slide()
-	
+		
 func enter_state():
-	pass
-
+	pass	
 func exit_state():
 	pass
 
@@ -69,7 +67,7 @@ func exit_state():
 
 func rotate_velocity(input: InputPackage, delta: float):
 	var facing_direction := visual.basis.z
-	# Gives you the angle from the visual facing vector to the input vector with a positive or negative sign indicating if its clockwise or anti-clockwise
+	# Gives you the angle IN RADIANS from the visual facing vector to the input vector with a positive or negative sign indicating if its clockwise or anti-clockwise
 	# input_dir.slide(UP) just flattens the vector, making sure it's in the x and z with y (up) being zero.  
 	var angle : float = facing_direction.signed_angle_to(Vector3(input.input_direction.x,0,input.input_direction.y), Vector3.UP) 
 	
@@ -77,12 +75,22 @@ func rotate_velocity(input: InputPackage, delta: float):
 	
 	if input.input_direction:
 		#if angle is bigger than angular speed
-		if abs(angle) >= ANGULAR_SPEED * delta and abs(angle) < 135:
+		if abs(angle) >= ANGULAR_SPEED * delta:
+			#if player.velocity.length() < 0.2 and abs(rad_to_deg(angle)) > 165:
+				#print("entering quick turn")
+				##player.velocity = Vector3.ZERO
+				##calculated_speed = 0.0
+				##visual.look_at(player.global_position - direction)
+				##calculated_angular_speed = move_toward(calculated_angular_speed,ANGULAR_SPEED_FROM_IDLE, ACCELERATION*delta)
+				##calculated_velocity = facing_direction.rotated(Vector3.UP, sign(angle) * ANGULAR_SPEED_FROM_IDLE * delta) * 0.1
+				#visual.look_at(facing_direction.rotated(Vector3.UP, sign(angle) * ANGULAR_SPEED_FROM_IDLE * delta))
+				
+			#else:
 			# rotate the velocity vector by angular speed each frame. So the turning is gradual. Sign(angle) just decides if clockwise or anti.
 			# Multiplied by "turn speed" because this is the move speed which you should be moving during a turn.
 			# Remember if there was no turning it would just be velocity = direction.x * TOP_SPEED. If the speed was always the same then TURN_SPEED here would also just be speed.
 			calculated_speed = move_toward(calculated_speed,TURN_SPEED, ACCELERATION*delta)
-			calculated_angular_speed = move_toward(calculated_angular_speed,ANGULAR_SPEED, ACCELERATION*delta)
+			calculated_angular_speed = move_toward(calculated_angular_speed,ANGULAR_SPEED, ACCELERATION*5*delta)
 			calculated_velocity = facing_direction.rotated(Vector3.UP, sign(angle) * calculated_angular_speed * delta) * calculated_speed
 		else:
 			#else rotate it by the angle. If the turn is even smaller than the angular speed then just turn it directly by that tiny bit?
@@ -93,7 +101,7 @@ func rotate_velocity(input: InputPackage, delta: float):
 			calculated_speed = move_toward(calculated_speed, 0.0, DECELERATION*delta)
 			calculated_angular_speed = move_toward(calculated_angular_speed, 0.0, DECELERATION*delta)
 			calculated_velocity = calculated_velocity.move_toward(Vector3.ZERO, DECELERATION*delta)
-	
+	#print("left quick turn")
 	# Calculated velocity will be determined based on the branching of the if statements, whatever it is gets assigned as the final player velocity here		
 	player.velocity = calculated_velocity
 
