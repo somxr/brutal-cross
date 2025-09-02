3e9ce839e2622125a1f6906ea02e3e71768a9e61
somxr <60410055+somxr@users.noreply.github.com>
2025-09-02 13:33:37 -0400

    more cleanup and comments

--- scenes/valerie.gd
+++ scenes/valerie.gd
@@ -8,9 +8,9 @@ var parent_model : PlayerModel
 var is_skating : float = false
 var is_turning: float = false
 
-var skate_state
+var skate_state : State
 
-var state_machine
+var state_machine : AnimationNodeStateMachinePlayback
 
 func _ready() -> void:
 	#animation_tree.set("parameters/state_anim/transition_request", "idle")
@@ -20,15 +20,15 @@ func _ready() -> void:
 func _physics_process(delta: float) -> void:
 	
 	if parent_model.current_state == parent_model.states["idle"]:
-		is_skating=false
+		pass
 		#animation_tree.set("parameters/state_anim/transition_request", "idle")
 	elif parent_model.current_state == parent_model.states["skate"]:
-		is_skating=true
 		skate_state = parent_model.current_state
 		animation_tree.set("parameters/skating/skate_direction_anim_blend/blend_amount", skate_state.current_turn_blend_value)
 		#animation_tree.set("parameters/state_anim/transition_request", "skate")
 		#print(state_machine.get_current_node())
 		
+		# hack solution to go back into skating immediately if input direction during transition to glide 
 		if state_machine.get_current_node() == "valerie_anim02_IDLE 2":	
 			if !skate_state.is_gliding:
 				state_machine.start("skating")
